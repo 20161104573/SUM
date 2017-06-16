f4ac5608ac24c867d4005ef2bafcc36f439f6874
20161104573 <a20161104573@admindeiMac-34.local>
2017-06-16 15:36:36 +0800

    读写文件
    从input中读取文件，写到output中

--- sum/main.c
+++ sum/main.c
@@ -10,6 +10,14 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    FILE *fr;
+    FILE *fw;
+    fr=fopen("//Users//a20161104573//Desktop//sum//input.txt","r+");
+    fw=fopen("//Users//a20161104573//Desktop//sum//output.txt","w");
+    int a,b,c;
+    fscanf(fr,"%d%d",&a,&b);
+    c=a+b;
+    printf("%d+%d=%d\n",a,b,c);
+    fprintf(fw,"%d+%d=%d\n",a,b,c);
     return 0;
 }
